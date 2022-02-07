4d5a3b8c087cabd5208b87fe5f76be69b01472e0
Kiernan2 <kiernan1994@gmail.com>
2022-02-07 14:55:24 -0800

    Added two checks and some tests for them

--- Triangle/Models/Triangle.cs
+++ Triangle/Models/Triangle.cs
@@ -2,12 +2,32 @@ namespace Triangle
 {
   public class Triangle
   {
+    public bool IsTriangle(int one, int two, int three)
+    {
+      if (one + two > three && one + three > two && two + three > one)
+      {
+        return true;
+      } else
+      {
+        return false;
+      }
+    }
     public bool IsEquilateral(int one, int two, int three)
     {
       if (one == two && two == three)
       {
         return true;
       } else 
+      {
+        return false;
+      }
+    }
+    public bool IsIsosceles(int one, int two, int three)
+    {
+      if (one == two || one == three || two == three)
+      {
+        return true;
+      } else
       {
         return false;
       }
--- TriangleTests/TriangleTests.cs
+++ TriangleTests/TriangleTests.cs
@@ -7,10 +7,29 @@ namespace Triangle
   public class LeapYearTests
   {
     [TestMethod]
+    public void IsRealTriangle()
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.IsTriangle(3,3,3));
+    }
+
+    [TestMethod]
     public void IsTriangle_Equilateral()
     {
       Triangle testEquilateral = new Triangle();
       Assert.AreEqual(true, testEquilateral.IsEquilateral(3,3,3));
+    }
+    [TestMethod]
+    public void IsTriangle_Not_Equilateral()
+    {
+      Triangle testEquilateral = new Triangle();
+      Assert.AreEqual(false, testEquilateral.IsEquilateral(4,3,1));
+    }
+    [TestMethod]
+    public void IsTriangle_Isosceles()
+    {
+      Triangle testIsosceles = new Triangle();
+      Assert.AreEqual(true, testIsosceles.IsIsosceles(2,2,4));
     }
   }
 }
